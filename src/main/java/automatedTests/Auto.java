d5cd1f91998273463376d696b397011707019787
JuanTwo779 <juanmangubat@gmail.com>
2024-12-19 15:13:46 +1100

    Initial commit

---
+++ src/main/java/automatedTests/Auto.java
@@ -0,0 +1,20 @@
+package automatedTests;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Auto {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(false)
+        );
+        Page page = browser.newPage();
+        page.navigate("https://www.google.com");
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+}
